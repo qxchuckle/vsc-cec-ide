81c8bd14f615f4507f5020a0b799ee92fa6608d6
qxchuckle <1934009145@qq.com>
2023-08-29 20:39:00 +0800

    优化代码

---
+++ src/sensitiveWords/CheckForSensitiveWords.ts
@@ -0,0 +1,35 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { createCodeActionProvider } from '../CodeActionProvider';
+import { detectSensitiveWords } from './DetectSensitiveWords';
+import { customSensitiveWords } from './CustomSensitiveWords';
+
+export function sensitiveWordDetectionInit(context: vscode.ExtensionContext) {
+  const sensitiveWordsFilePath = getSensitiveWordsFilePath(context);
+  fs.readFile(sensitiveWordsFilePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      vscode.window.showErrorMessage('读取敏感词文件出错！');
+      return;
+    }
+    detectSensitiveWords(context, data); // 初始化对敏感词的检测
+    customSensitiveWords(context); // 自定义敏感词
+    // 注册 CodeActionProvider，指定诊断来源
+    const codeActionProvider = createCodeActionProvider('敏感词检测');
+  });
+}
+
+// 获取敏感词文件路径
+function getSensitiveWordsFilePath(context: vscode.ExtensionContext): string {
+  let sensitiveWordsFilePath = path.join(context.extensionPath, 'resource', 'text', 'SensitiveWordsEncryption.txt');
+  const customSensitiveWordsPath = path.join(context.extensionPath, 'resource', 'text', 'CustomSensitiveWords.txt');
+  try {
+    const data = fs.readFileSync(customSensitiveWordsPath, 'utf8');
+    // 如果自定义敏感词文件不为空，则使用该文件
+    if (!(data.trim() === "")) {
+      sensitiveWordsFilePath = customSensitiveWordsPath;
+    }
+  } catch (err) { }
+  return sensitiveWordsFilePath;
+}
---
+++ src/sensitiveWords/DetectSensitiveWords.ts
@@ -0,0 +1,143 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import Mint from 'mint-filter';
+import { decrypt } from '../utils/EncryptionAndDecryption';
+
+// 创建一个诊断集合对象
+const diagnosticCollection = vscode.languages.createDiagnosticCollection('sensitiveWords');
+// 记录文件监听器
+const documentListeners: { [key: string]: vscode.Disposable } = {};
+// 记录文件状态
+const fileStates: { [key: string]: boolean } = {};
+
+export function detectSensitiveWords(context: vscode.ExtensionContext, data: string){
+  const mint = initializeSensitiveWords(data, 'chuckle');
+    const markCommand = vscode.commands.registerCommand('cec-ide.mark-sensitive-words', () => {
+      markSensitiveWords(mint);
+    });
+    const stopCommand = vscode.commands.registerCommand('cec-ide.stop-mark-sensitive-words', () => {
+      stopMarkSensitiveWords();
+    });
+    context.subscriptions.push(markCommand, stopCommand);
+    // 取消文档更改事件的监听
+    vscode.workspace.onDidCloseTextDocument(closedDocument => {
+      cleanUpDocument(closedDocument, () => {
+        diagnosticCollection.delete(closedDocument.uri);
+      });
+    });
+}
+
+// 获取敏感词
+function initializeSensitiveWords(data: string, password: string) {
+  const decryptedText = decrypt(data, password); // 解密
+  const sensitiveWordsArray = decryptedText.split('\n').map((word: string) => word.trim());
+  const mint = new Mint(sensitiveWordsArray);
+  return mint;
+}
+
+// 标记敏感词
+function markSensitiveWords(mint: Mint){
+  const editor = vscode.window.activeTextEditor;
+  if (editor && fileStates[editor.document.fileName]) {
+    return; // 已经检测中的文件不重复进行检测
+  }
+  if (editor && mint) {
+    activateDocumentChangeListener(editor.document, mint); // 注册文档更改监听器
+    detectionMarkDiagnosis(editor, mint);
+    fileStates[editor.document.fileName] = true; // 记录文件状态为已检测
+  } else {
+    vscode.window.showErrorMessage('没有活动的文本编辑器。');
+  }
+}
+
+// 激活文档更改监听器
+function activateDocumentChangeListener(document: vscode.TextDocument, mint: Mint) {
+  if (!documentListeners[document.fileName]) {
+    const listener = vscode.workspace.onDidChangeTextDocument(event => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor && editor.document === document && mint) {
+        detectionMarkDiagnosis(editor, mint);
+      }
+    });
+    documentListeners[document.fileName] = listener;
+  }
+}
+
+// 停止检测敏感词
+function stopMarkSensitiveWords(){
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    stopSensitiveWordDetection(editor.document);
+  }
+}
+
+// 停止敏感词检测、清除标记
+function stopSensitiveWordDetection(document: vscode.TextDocument) {
+  cleanUpDocument(document, () => {
+    diagnosticCollection.delete(document.uri);
+    vscode.window.showInformationMessage(`已停止检测敏感词。`);
+  });
+}
+
+// 进行文档清理操作
+function cleanUpDocument(document: vscode.TextDocument, callback?: () => void) {
+  if (fileStates[document.fileName]) {
+    delete fileStates[document.fileName];
+    if (documentListeners[document.fileName]) {
+      documentListeners[document.fileName].dispose();
+      delete documentListeners[document.fileName];
+    }
+    if (callback) {
+      callback();
+    }
+  }
+}
+
+// 检测敏感词、标记诊断
+function detectionMarkDiagnosis(editor: vscode.TextEditor, mint: Mint) {
+  const document = editor.document;
+  const text = document.getText();
+  const sensitiveWords = new Set(mint.filter(text).words); // 使用 Set 来存储唯一的敏感词
+  const diagnostics: vscode.Diagnostic[] = [];
+
+  for (const word of sensitiveWords) {
+    const wordRegExp = new RegExp(word, 'gi');
+    let match;
+
+    while ((match = wordRegExp.exec(text)) !== null) {
+      const startPos = document.positionAt(match.index);
+      const endPos = document.positionAt(match.index + word.length);
+      const range = new vscode.Range(startPos, endPos);
+
+      const diagnostic = new vscode.Diagnostic(range, '敏感词', vscode.DiagnosticSeverity.Warning);
+      diagnostic.source = '敏感词检测';
+      diagnostic.relatedInformation = [
+        new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, range), `${word}`)
+      ];
+
+      diagnostics.push(diagnostic);
+    }
+  }
+
+  diagnosticCollection.delete(document.uri);
+
+  if (diagnostics.length > 0) {
+    diagnosticCollection.set(document.uri, diagnostics);
+    if (!fileStates[editor.document.fileName]) {
+      const stopAction: vscode.MessageItem = { title: '停止检测' };
+      vscode.window.showInformationMessage(
+        `开始检测${path.basename(document.fileName)}，共有${diagnostics.length}个敏感词。`,
+        stopAction
+      ).then((selectedAction) => {
+        if (selectedAction === stopAction) {
+          stopSensitiveWordDetection(editor.document);
+        }
+      });
+    }
+  } else {
+    vscode.window.showInformationMessage(`${path.basename(document.fileName)}中已没有敏感词，停止检测。`);
+    cleanUpDocument(document);
+  }
+}
+
+
