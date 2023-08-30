677a9d4af6ef3365c5626e7ac7ebeabb8a02b58e
qxchuckle <1934009145@qq.com>
2023-08-30 16:06:28 +0800

    优化代码，让一键快速替换仅在敏感词警告标记上应用

--- src/sensitiveWords/CodeActionProvider.ts
+++ src/sensitiveWords/CodeActionProvider.ts
@@ -8,54 +8,57 @@ export function createCodeActionProvider(diagnosticSource: string, diagnosticCol
       context: vscode.CodeActionContext,
       token: vscode.CancellationToken
     ): vscode.ProviderResult<vscode.CodeAction[]> {
+      // 用于快速修复的动作数组
       const actions: vscode.CodeAction[] = [];
-      for (const diagnostic of context.diagnostics) {
-        // 仅处理指定的诊断来源
-        if (diagnostic.source === diagnosticSource) {
-          const fix = new vscode.CodeAction(`将敏感词 ${document.getText(diagnostic.range)} 替换为***`, vscode.CodeActionKind.QuickFix);
-          fix.edit = new vscode.WorkspaceEdit();
-          fix.edit.replace(document.uri, diagnostic.range, '*'.repeat(diagnostic.range.end.character - diagnostic.range.start.character));
-          fix.diagnostics = [diagnostic];
-          actions.push(fix);
-        }
-      }
+      const fixes = context.diagnostics.map(diagnostic => {
+        if (!(diagnostic.source === diagnosticSource)) { return null; }
+        const fix = new vscode.CodeAction(`将敏感词 ${document.getText(diagnostic.range)} 替换为***`, vscode.CodeActionKind.QuickFix);
+        const newText = '*'.repeat(diagnostic.range.end.character - diagnostic.range.start.character);
+        const edit = new vscode.WorkspaceEdit();
+        edit.replace(document.uri, diagnostic.range, newText);
+        fix.edit = edit;
+        fix.diagnostics = [diagnostic];
+        return fix;
+      }).filter((fix): fix is vscode.CodeAction => fix !== null);
+      // 如果没有可修复的，也就不用往后执行了
+      if(fixes.length === 0) { return []; }
+      actions.push(...fixes);
 
-      // 这几行不能放到循环中，否则有多个敏感词位置重叠的时候就会显示多个 一键修复 按钮
+      // 获取当前文档的诊断信息集合
       const diagnosticsOnCurrentDocument = diagnosticCollection.get(document.uri);
-      if (diagnosticsOnCurrentDocument) {
+
+      // 仅处理指定的诊断来源
+      if (diagnosticsOnCurrentDocument ) {
+        // 创建一键修复所有敏感词的 CodeAction
         const fixAll = new vscode.CodeAction('一键修复所有敏感词', vscode.CodeActionKind.QuickFix);
         fixAll.edit = new vscode.WorkspaceEdit();
 
-        // 如果敏感词之间有重叠部分，直接替换就替换不了会报错，所以我们要解决一下重叠部分的问题
-        const diagnosticsSorted = Array.from(diagnosticsOnCurrentDocument).sort((a, b) => a.range.start.line === b.range.start.line ? a.range.start.character - b.range.start.character : a.range.start.line - b.range.start.line); // 先排序，便于处理重叠
-        const ranges: vscode.Range[] = [];
-        let rangeStartPos: vscode.Position = new vscode.Position(0, 0), rangeEndPos: vscode.Position = new vscode.Position(0, 0), isOverlapped = false; // 用 rangeStartPos 和 rangeEndPos 来记录敏感词的开始和结束位置
-        for (let i = 0; i < diagnosticsSorted.length; ++i) {
-          if (diagnosticsSorted[i].range.start.line === diagnosticsSorted[i + 1]?.range?.start?.line // 同一行
-            && i !== diagnosticsSorted.length - 1 // 不是最后一个敏感词，最后一个敏感词后面肯定不会有连着的了，直接push就好了
-            && diagnosticsSorted[i].range.end.character >= diagnosticsSorted[i + 1].range.start.character // 前一个结束的位置比后一个开始的位置大，存在重叠
-          ) {
-            if (!isOverlapped) {
-              // 之前没有重叠过的
-              rangeStartPos = diagnosticsSorted[i].range.start;
-              isOverlapped = true;
-            }
-            rangeEndPos = diagnosticsSorted[i + 1].range.end; // 更新结束位置
-          } else {
-            if (isOverlapped) {
-              // 之前重叠过，到这不重叠了
-              ranges.push(new vscode.Range(rangeStartPos, rangeEndPos));
-              isOverlapped = false;
-            } else {
-              ranges.push(diagnosticsSorted[i].range);
-            }
+        // 敏感词按位置排序，以便处理重叠
+        const diagnosticsSorted = Array.from(diagnosticsOnCurrentDocument)
+          .sort((a, b) => a.range.start.compareTo(b.range.start));
+
+        let currentRange: vscode.Range | null = null;
+
+        for (const diagnostic of diagnosticsSorted) {
+          if (!currentRange) {
+            // 如果当前没有重叠的范围，直接设置为当前诊断范围
+            currentRange = diagnostic.range;
+          } else if (currentRange.end.isBeforeOrEqual(diagnostic.range.start)) {
+            // 如果当前重叠范围的结束位置在当前诊断范围之前，添加之前的范围并重新开始记录
+            fixAll.edit!.replace(document.uri, currentRange, '*'.repeat(currentRange.end.character - currentRange.start.character));
+            currentRange = diagnostic.range;
+          } else if (currentRange.end.isBefore(diagnostic.range.end)) {
+            // 如果当前重叠范围的结束位置在当前诊断范围内，更新结束位置
+            currentRange = new vscode.Range(currentRange.start, diagnostic.range.end);
           }
         }
 
-        ranges.forEach(range => {
-          fixAll.edit!.replace(document.uri, range, '*'.repeat(range.end.character - range.start.character));
-        });
+        // 处理最后一个重叠范围
+        if (currentRange) {
+          fixAll.edit!.replace(document.uri, currentRange, '*'.repeat(currentRange.end.character - currentRange.start.character));
+        }
 
+        // 将修复操作应用到 CodeAction
         fixAll.diagnostics = [...diagnosticsOnCurrentDocument];
         actions.push(fixAll);
       }
