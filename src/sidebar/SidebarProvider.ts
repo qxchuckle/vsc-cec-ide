0844a0c24e558201398899a81617b0563f4f04b3
qxchuckle <1934009145@qq.com>
2023-09-03 14:07:52 +0800

    允许在设置中自定义侧边栏视图部分信息

--- src/sidebar/SidebarProvider.ts
+++ src/sidebar/SidebarProvider.ts
@@ -5,13 +5,9 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
   _view?: vscode.WebviewView;
   _doc?: vscode.TextDocument;
   private _htmlFileName: string;
-  private _cssFileName: string;
-  private _jsFileName: string;
 
-  constructor(private readonly _extensionUri: vscode.Uri, htmlFileName: string, cssFileName: string, jsFileName: string) {
+  constructor(private readonly _extensionUri: vscode.Uri, htmlFileName: string) {
     this._htmlFileName = htmlFileName;
-    this._cssFileName = cssFileName;
-    this._jsFileName = jsFileName;
   }
 
   public resolveWebviewView(webviewView: vscode.WebviewView) {
@@ -31,14 +27,33 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
   }
 
   private _getHtmlContent(webview: vscode.Webview) {
-    const htmlFilePath = vscode.Uri.joinPath(this._extensionUri,'resource', 'page', this._htmlFileName);
+    const htmlFilePath = vscode.Uri.joinPath(this._extensionUri, 'resource', 'page', this._htmlFileName);
     let htmlContent = fs.readFileSync(htmlFilePath.fsPath, "utf-8");
     const extensionPath = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri)
     );
-
+    const sidebarConfig = vscode.workspace.getConfiguration('cec-ide-sidebar');
+    const activateVip = sidebarConfig.get('activateVip');
+    const username = sidebarConfig.get('username');
+    const deadline = activateVip ? `会员截止日期：${sidebarConfig.get('deadline')}` : `点击下方按钮，激活会员`;
+    const activation = activateVip ? `续费会员` : `激活会员`;
+    const vip = `<div class="vip-box${activateVip ? '' : ' no-vip'}">
+  <div class="vip">${activateVip ? sidebarConfig.get('grade') : 'VIP0'}</div>
+</div>`;
     htmlContent = htmlContent.replace(
-      /#extensionPath/g,
+      /#username/g, // 替换用户名
+      `${username}`
+    ).replace(
+      /#deadline/g, // 替换会员截止日期
+      `${deadline}`
+    ).replace(
+      /#activation/g, // 替换充值会员按钮文本
+      `${activation}`
+    ).replace(
+      /#VIP/g, // 替换会员等级
+      `${vip}`
+    ).replace(
+      /#extensionPath/g, // 替换资源根路径
       `${extensionPath}`
     );
 
--- src/sidebar/sidebarInit.ts
+++ src/sidebar/sidebarInit.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { SidebarProvider } from "./SidebarProvider";
 
 export function sidebarInit(context: vscode.ExtensionContext) {
-	const sidebarPanel = new SidebarProvider(context.extensionUri, 'cec-sidebar-main.html', 'cec-sidebar-main.css', 'cec-sidebar-main.js');
+	const sidebarPanel = new SidebarProvider(context.extensionUri, 'cec-sidebar-main.html');
 	context.subscriptions.push(
 		vscode.window.registerWebviewViewProvider("cec-sidebar-main", sidebarPanel)
 	);
